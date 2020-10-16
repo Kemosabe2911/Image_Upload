a1ffc221f01cb108467ca77b2d272152d397d969
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-16 18:23:27 +0530

    update server.js

--- server.js
+++ server.js
@@ -6,8 +6,17 @@ const path= require('path');
 //Init express
 const app= express();
 
+//EJS
+app.set('view engine','ejs');
+
+//Public Folder
+app.use(express.static('./public')); 
+
 const PORT= process.env.PORT || 5000;
 
+app.get('/',(req,res)=>{
+    res.render('index');
+})
 
 
 app.listen(PORT, () =>{
